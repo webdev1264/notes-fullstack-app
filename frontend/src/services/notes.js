cf9680b67726b6efea526dc42455909a5a8afc8d
webdev <webdev1264@gmail.com>
2023-10-02 01:18:48 +0200

    Restructure projects directories

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/notes";
+const baseUrl = "/api/notes";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
